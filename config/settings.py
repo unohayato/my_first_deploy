4411654b63fc629ba340d575170e052c227b5065
unohayato <mamidori1p1p@gmail.com>
2023-01-24 17:19:13 +0900

    デプロイ設定

--- config/settings.py
+++ config/settings.py
@@ -122,10 +122,10 @@ except ImportError:
 # ローカル用設定
 if DEBUG:
     ALLOWED_HOSTS = ['*']
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if not DEBUG:
+else:
     import environ
     env = environ.Env()
     env.read_env(os.path.join(BASE_DIR,'.env'))
@@ -133,12 +133,12 @@ if not DEBUG:
     SECRET_KEY = env('SECRET_KEY')
     ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-    EMAIL_USE_TLS = True
+    #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    #EMAIL_HOST = 'smtp.gmail.com'
+    #EMAIL_PORT = 587
+    #EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    #EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    #EMAIL_USE_TLS = True
 
     STATIC_ROOT = '/usr/share/nginx/html/static'
     MEDIA_ROOT = '/usr/share/nginx/html/media'
